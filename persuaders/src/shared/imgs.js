4c05596f86f3cba17ffcd88ab2538ea6e9c71902
basilleeCMU <112532066+basilleeCMU@users.noreply.github.com>
2022-12-01 01:54:13 -0500

    restructuring changes

--- persuaders/src/shared/imgs.js
+++ persuaders/src/shared/imgs.js
@@ -1,11 +1,39 @@
 import Pot from "../assets/imgs/pot_normal.svg";
 import PotLit from "../assets/imgs/pot_lit.svg";
-import Apple from "../assets/imgs/apple_slice.svg";
-import Potato from "../assets/imgs/Potato.svg";
-import Curry from "../assets/imgs/curry.svg";
-import Carrot from "../assets/imgs/carrot.svg";
-import Banana from "../assets/imgs/banana.svg";
-import Recipe from "../assets/imgs/recipe_book.svg";
+
+//Curry
+import EnvHiro from "../assets/imgs/curry/environment_hiro.svg";
+import CurryFridge from "../assets/imgs/curry/fridge.svg";
+import CurryFridgeOpen from "../assets/imgs/curry/fridge_open.svg";
+import CurryFridgeDoor from "../assets/imgs/curry/fridge_door.svg";
+import CurryLowerCabinet from "../assets/imgs/curry/lower_cabinet.svg";
+import CurryLowerCabinetOpen from "../assets/imgs/curry/lower_cabinet_open.svg";
+import CurryLowerCabinetDoor from "../assets/imgs/curry/lower_cabinet_door.svg";
+import CurrySink from "../assets/imgs/curry/sink_cabinet.svg";
+import CurrySinkOpen from "../assets/imgs/curry/sink_cabinet_open.svg";
+import CurrySinkDoor from "../assets/imgs/curry/sink_cabinet_door.svg";
+import CurryUpperCabinet from "../assets/imgs/curry/upper_cabinet.svg";
+import CurryUpperCabinetOpen from "../assets/imgs/curry/upper_cabinet_open.svg";
+import CurryUpperCabinetDoor from "../assets/imgs/curry/upper_cabinet_door.svg";
+import Apple from "../assets/imgs/curry/apple_slice.svg";
+import Potato from "../assets/imgs/curry/potato.svg";
+import Curry from "../assets/imgs/curry/curry.svg";
+import Carrot from "../assets/imgs/curry/carrot.svg";
+import Banana from "../assets/imgs/curry/banana.svg";
+
+import Recipe from "../assets/imgs/curry/recipe_book.svg";
+
+//Salad
+import EnvMatt from "../assets/imgs/salad/environment_matt.svg";
+import Cabinet from "../assets/imgs/salad/cabinet.svg";
+import Fridge from "../assets/imgs/salad/fridge.svg";
+import KitchenSink from "../assets/imgs/salad/kitchen_sink.svg";
+import LargeCabinet from "../assets/imgs/salad/large_cabinet.svg";
+import CabinetOpen from "../assets/imgs/salad/cabinet_open.svg";
+import FridgeOpen from "../assets/imgs/salad/fridge_open.svg";
+import KitchenSinkOpen from "../assets/imgs/salad/kitchen_sink_open.svg";
+import LargeCabinetOpen from "../assets/imgs/salad/large_cabinet_open.svg";
+
 
 const images = {
   Pot: Pot,
@@ -16,6 +44,28 @@ const images = {
   Carrot: Carrot,
   Banana: Banana,
   Recipe: Recipe,
+  EnvHiro: EnvHiro,
+  CurryFridge: CurryFridge,
+  CurryFridgeOpen: CurryFridgeOpen,
+  CurryFridgeDoor: CurryFridgeDoor,
+  CurryLowerCabinet: CurryLowerCabinet,
+  CurryLowerCabinetOpen: CurryLowerCabinetOpen,
+  CurryLowerCabinetDoor: CurryLowerCabinetDoor,
+  CurrySink: CurrySink,
+  CurrySinkDoor: CurrySinkDoor,
+  CurrySinkOpen: CurrySinkOpen,
+  CurryUpperCabinet: CurryUpperCabinet,
+  CurryUpperCabinetOpen: CurryUpperCabinetOpen,
+  CurryUpperCabinetDoor: CurryUpperCabinetDoor,
+  EnvMatt: EnvMatt,
+  Cabinet: Cabinet,
+  Fridge: Fridge,
+  KitchenSink: KitchenSink,
+  LargeCabinet: LargeCabinet,
+  CabinetOpen: CabinetOpen,
+  FridgeOpen: FridgeOpen,
+  KitchenSinkOpen: KitchenSinkOpen,
+  LargeCabinetOpen: LargeCabinetOpen,
 };
 
 export default images;
